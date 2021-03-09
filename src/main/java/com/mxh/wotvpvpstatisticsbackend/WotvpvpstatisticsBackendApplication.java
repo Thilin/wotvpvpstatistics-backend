bb47110f4b9c75d4cd2ef6fd106433fd00359a7c
Thilin <fernando_cauper@hotmail.com>
2021-03-09 19:56:25 -0400

    first commit

---
+++ src/main/java/com/mxh/wotvpvpstatisticsbackend/WotvpvpstatisticsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mxh.wotvpvpstatisticsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WotvpvpstatisticsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WotvpvpstatisticsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mxh/wotvpvpstatisticsbackend/WotvpvpstatisticsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mxh.wotvpvpstatisticsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WotvpvpstatisticsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
