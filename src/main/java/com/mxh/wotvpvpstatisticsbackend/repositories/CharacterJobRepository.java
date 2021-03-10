080dc568bab5d2ee93d43e3b56e408a9b295d8da
Thilin <fernando_cauper@hotmail.com>
2021-03-10 17:02:32 -0400

    feat: added CharacterJob mapping

---
+++ src/main/java/com/mxh/wotvpvpstatisticsbackend/repositories/CharacterJobRepository.java
@@ -0,0 +1,7 @@
+package com.mxh.wotvpvpstatisticsbackend.repositories;
+
+import com.mxh.wotvpvpstatisticsbackend.models.CharacterJob;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CharacterJobRepository extends JpaRepository<CharacterJob, Long> {
+}
